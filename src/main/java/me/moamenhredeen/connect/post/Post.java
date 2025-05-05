d9c503aa200e78c1f7e183aaf50737097ebcbe17
Moamen Hraden <moamen.hraden@primion.eu>
2025-05-05 21:04:10 +0200

    add mssing annotation

--- src/main/java/me/moamenhredeen/connect/post/Post.java
+++ src/main/java/me/moamenhredeen/connect/post/Post.java
@@ -1,9 +1,11 @@
 package me.moamenhredeen.connect.post;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
 
 import java.time.LocalDateTime;
 
+@Entity
 public class Post extends PanacheEntity {
     String title;
     String content;
